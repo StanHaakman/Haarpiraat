18abe6708abab0ea2787fccbc93040e51f7ce1dc
StanHaakman <34091804+StanHaakman@users.noreply.github.com>
2018-11-09 15:34:31 +0100

    initial Commit

---
+++ Navbar/header.php
@@ -0,0 +1,54 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Haarpiraat</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg"
+          crossorigin="anonymous">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.css" rel="stylesheet">
+    <link href="//cdnjs.cloudflare.com/ajax/libs/normalize/3.0.2/normalize.min.css" rel="stylesheet">
+    <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"
+            integrity="sha384-tsQFqpEReu7ZLhBV2VZlAu7zcOV+rXbYlF2cqB8txI/8aZajjp4Bqd+V6D5IgvKT"
+            crossorigin="anonymous">
+    </script>
+    <script src="script.js"></script>
+</head>
+<body>
+<nav>
+    <ul class="navul">
+        <li id="logo"><a href="">Haarpiraat</a></li>
+        <li><a href="">Home</a></li>
+        <li><a href="">Over ons</a></li>
+        <li><a href="">Tarieven</a></li>
+        <li><a href="">Curlsys</a></li>
+        <li><a href="">Instagram</a></li>
+        <li><a href="">Contact</a></li>
+    </ul>
+    <div class="button">
+        <a class="btn-open" href="#"></a>
+    </div>
+</nav>
+<div class="overlay">
+    <div class="wrap">
+        <ul class="wrap-nav">
+            <ul class="overlay-ul">
+                <li><a href="">Home</a></li>
+                <li><a href="">Over ons</a></li>
+                <li><a href="">Tarieven</a></li>
+                <li><a href="">Curlsys</a></li>
+                <li><a href="">Instagram</a></li>
+                <li><a href="">Contact</a></li>
+            </ul>
+        </ul>
+    </div>
+</div>
+
+
+</body>
+</html>
